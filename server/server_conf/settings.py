e9b6cea2d3105355967216629833589c988e59a3
Maria29-b <belaidimaria29@gmail.com>
2025-05-13 12:45:33 +0000

    Ajout de PrometheusAfterMiddleware dans le fichier setting.py  pour finaliser la collecte des metriques

--- server/server_conf/settings.py
+++ server/server_conf/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'server_conf.urls'
